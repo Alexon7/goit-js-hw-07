638015855419cfb5437e7ba1202dae9332789fa2
Alexon7 <alenkaalexon@gmail.com>
2023-03-02 11:21:50 +0100

    task 2(fix)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
-const galleryMarkup = createGallery(galleryItems);
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.insertAdjacentHTML('beforeend', createGallery(galleryItems));
 
 
 function createGallery(galleryItems) {
@@ -20,7 +20,7 @@ function createGallery(galleryItems) {
 //     const createGalleryEl = galleryItems.map(({ preview, original, description }) => {
 //         return `<a class="gallery__item" href="${original}">
 //   <img class="gallery__image" src="${preview}" alt="${description}" />
-//  </a>`}).join("");
+// </a>`}).join('');
     
 //     galleryContainer.insertAdjacentHTML('beforeend', createGalleryEl);
 
