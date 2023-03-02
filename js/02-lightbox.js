c74b36709241b0b9ab7934fe209893ead26673c4
Alexon7 <alenkaalexon@gmail.com>
2023-03-02 09:57:51 +0100

    after fix mentor

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,19 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
-// const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+// const galleryMarkup = createGallery(galleryItems);
 
 // galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
 
-
-galleryContainer.innerHTML = galleryItems.map(({preview, original, description })=> {
-        return `<a class="gallery__item" href="${original}">
+// function createGallery(galleryItems) {
+//     return galleryItems.map(({preview, original, description })=> {
+//         return `<a class="gallery__item" href="${original}">
+//   <img class="gallery__image" src="${preview}" alt="${description}" />
+// </a>`;
+//     }).join('');
+    
+// }
+function createGallery() {
+    const createGalleryEl = galleryItems.map(({ preview, original, description }) => {return `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
-    }).join('');
-   
+ </a>`;
+            
+    }).join("");
+    
+    galleryContainer.insertAdjacentHTML('beforeend', createGalleryEl);
 
+    }
+ 
 
  new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
